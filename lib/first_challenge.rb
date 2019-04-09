4bc99d49e5ca24f5936b1376423facc9ff32035a
Henry Koehler <henryk1229@gmail.com>
2019-04-09 16:41:36 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,8 @@ def first_challenge
   }
 
  contacts.each do |person, data|
-   data.each do |key, value| binding.pry 
+   data.each do |key, value| 
+     binding.pry 
      value.each do |key, value|
       value.delete_if { |value| value == "strawberry" }
     end 
